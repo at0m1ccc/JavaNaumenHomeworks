2e2f297f1e823f310e185c3fd702b54a15e7d15f
at0m1c <konstantintatarinov177@gmail.com>
2025-03-10 21:07:46 +0500

    Complete task3

---
+++ src/main/java/org/example/task3/Employee.java
@@ -0,0 +1,30 @@
+package org.example.task3;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Employee {
+    private String fullName;
+    private Integer age;
+    private String department;
+    private Double salary;
+
+    public Employee(String fullName, Integer age, String department, Double salary) {
+        this.fullName = fullName;
+        this.age = age;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", age=" + age +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
